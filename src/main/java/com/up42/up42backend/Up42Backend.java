2d987fa550901c24354c2ee21b6d5bc239b4d68e
Faical Jouini <fjouini@gmail.com>
2021-02-08 19:24:15 +0100

    Initial commit: Base application

---
+++ src/main/java/com/up42/up42backend/Up42Backend.java
@@ -0,0 +1,13 @@
+package com.up42.up42backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Up42Backend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Up42Backend.class, args);
+	}
+
+}
