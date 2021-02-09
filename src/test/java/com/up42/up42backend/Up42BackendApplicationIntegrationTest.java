7d437fc23749b6afbe3b9a0d8f00453efa8600ec
Faical Jouini <fjouini@gmail.com>
2021-02-09 13:06:05 +0100

    Add integration tests

--- src/test/java/com/up42/up42backend/Up42BackendApplicationIntegrationTest.java
+++ src/test/java/com/up42/up42backend/Up42BackendApplicationIntegrationTest.java
@@ -1,12 +1,18 @@
 package com.up42.up42backend;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.up42.up42backend.model.Feature;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class Up42BackendApplicationIntegrationTest {
     private RestTemplate restTemplate;
@@ -17,29 +23,48 @@ public class Up42BackendApplicationIntegrationTest {
     }
 
     @Test
-    public void testGetFeatures() {
+    public void testGetFeatures_returnsAllFeatures() {
         ResponseEntity<Feature[]> response = restTemplate.getForEntity("http://localhost:8080/features", Feature[].class);
-        Assertions.assertNotNull(response.getBody());
+        assertNotNull(response.getBody());
+        assertEquals(14, ((Feature[])response.getBody()).length);
     }
 
     @Test
-    public void testGetFeatureById() {
-        ResponseEntity<Feature> response = restTemplate.getForEntity("http://localhost:8080/features/123", Feature.class);
+    public void testGetFeatureById_returnsFeature() {
+        ResponseEntity<Feature> response = restTemplate.getForEntity("http://localhost:8080/features/39c2f29e-c0f8-4a39-a98b-deed547d6aea", Feature.class);
         Feature feature = response.getBody();
-        Assertions.assertNotNull(feature);
-        Assertions.assertEquals("id" , feature.getId());
-        Assertions.assertEquals(123l , feature.getTimestamp());
-        Assertions.assertEquals(456l, feature.getBeginViewingDate());
-        Assertions.assertEquals(789l, feature.getEndViewingDate());
-        Assertions.assertEquals("missionName", feature.getMissionName());
+        assertNotNull(feature);
+        assertEquals("39c2f29e-c0f8-4a39-a98b-deed547d6aea" , feature.getId());
+        assertEquals(1554831167697L , feature.getTimestamp());
+        assertEquals(1554831167697L, feature.getBeginViewingDate());
+        assertEquals(1554831202043L, feature.getEndViewingDate());
+        assertEquals("Sentinel-1B", feature.getMissionName());
     }
 
     @Test
-    public void testGetFeatureQuickLook() {
-        ResponseEntity<byte[]> response = restTemplate.getForEntity("http://localhost:8080/features/123/quicklook", byte[].class);
-        byte[] image = response.getBody();
-        Assertions.assertNotNull(image);
-        Assertions.assertArrayEquals(new byte[]{1, 2, 3}, image);
+    public void testGetFeatureById_whenInvalidId_returns404() {
+        try {
+            restTemplate.getForEntity("http://localhost:8080/features/invalid", Feature.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
+        }
     }
 
+    @Test
+    public void testGetFeatureQuickLook_returnsImage() {
+        ResponseEntity<byte[]> response = restTemplate.getForEntity("http://localhost:8080/features/39c2f29e-c0f8-4a39-a98b-deed547d6aea/quicklook", byte[].class);
+        byte[] image = response.getBody();
+        assertNotNull(image);
+        try {
+            ImageIO.read(new ByteArrayInputStream(image));
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetFeatureQuickLook_whenFeatureHasNoQuicklook_returns204() {
+        ResponseEntity<byte[]> response = restTemplate.getForEntity("http://localhost:8080/features/b0d3bf6a-ff54-49e0-a4cb-e57dcb68d3b5/quicklook", byte[].class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    }
 }
